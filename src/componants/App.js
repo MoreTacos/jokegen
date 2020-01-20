bf335cb8944ba2c52d4d067597bfa4e57d6de7f1
Davide Radaelli <daviderad@outlook.com>
2020-01-20 16:37:33 -0500

    added jokesdisplay fully

--- src/componants/App.js
+++ src/componants/App.js
@@ -4,18 +4,8 @@ import ParamsBar from './ParamsBar';
 import Favorites from './Favorites';
 import JokesDisplay from './JokesDisplay';
 
-jokes
-  .get('jokes/random', {
-    params: {
-      foo: 'bar'
-    }
-  })
-  .then(res => console.log(res));
 class App extends React.Component {
   state = { jokes: [] };
-  componentDidUpdate() {
-    console.log(this.state.jokes);
-  }
   render() {
     return (
       <div>
--- src/componants/JokesDisplay.js
+++ src/componants/JokesDisplay.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const JokesDisplay = params => {
-  return <div>JOKES</div>;
+const JokesDisplay = props => {
+  if (props.jokes.data) {
+    const list = props.jokes.data.value.map(el => {
+      return <div>{el.joke}</div>;
+    });
+    return <div>{list}</div>;
+  } else {
+    return <div>Press Generate</div>;
+  }
 };
 
 export default JokesDisplay;
